342745afc2b6ce29192a897f43e350758a878873
Yuryi Baravy <ybaravy@gmail.com>
2019-12-08 18:39:59 +0300

    Initial commit

---
+++ components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Header } from 'react-native-elements';
+import { StyleSheet } from "react-native";
+
+const HeaderWrapper = () => {
+    return (
+        <Header
+            containerStyle={styles.header}
+            placement="right"
+            leftComponent={{ text: 'test', style: { color: '#fff' } }}
+            centerComponent={{ icon: 'search', color: '#fff' }}
+        />
+    )
+};
+
+export default HeaderWrapper;
+
+const styles = StyleSheet.create({
+    header: {
+        height: 80,
+        alignItems: 'center',
+    }
+});
