0cc62c8177278cef659236982dd89a656215b29e
Yuryi Baravy <ybaravy@gmail.com>
2020-03-03 13:50:06 +0300

    Improve saving to S3

--- screens/DoctorScreen.js
+++ screens/DoctorScreen.js
@@ -1,9 +1,44 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, AsyncStorage } from 'react-native';
 import { Button } from 'react-native-elements';
 import Header from "../components/Header";
 import DoctorsCalendar from '../components/DoctorsCalendar';
 import DoctorTimePicker from '../components/DoctorTimePicker';
+
+const bUrl = 'https://rjjt56u7fb.execute-api.eu-central-1.amazonaws.com/stage/save';
+
+const saveObj = async (obj) => {
+  const user = JSON.parse(await AsyncStorage.getItem('@user'));
+  const jsonObj = {
+    ...obj,
+    user,
+  };
+  let objectKey = 'default';
+  if (user.fullName) {
+    objectKey = user.fullName.split(' ').join('_');
+  } else if (user.phone) {
+    objectKey = user.phone;
+  }
+
+  const event = {
+    objectKey,
+    jsonObj,
+  };
+
+  const params = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ params: event }),
+  };
+  try {
+    let response = await fetch(bUrl, params);
+    let responseJson = await response.json();
+    console.log(responseJson);
+  } catch (error) {
+    console.error(error);
+  }
+  return null;
+};
 
 const DoctorScreen = (props) => {
   const [ time, setTime ] = useState(null);
@@ -25,7 +60,10 @@ const DoctorScreen = (props) => {
         <DoctorTimePicker setTime={setTime}/>
         <View style={styles.buttonWrapper}>
           <View style={styles.buttonStyles}>
-            <Button title='Save Data'/>
+            <Button
+              title='Save Data'
+              onPress={() => saveObj({ time, date, doctor })}
+            />
           </View>
         </View>
       </View>
